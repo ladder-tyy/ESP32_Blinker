2586c479409ad51c2970b6f402c8d2875362e341
ladder-tyy <1658406095@qq.com>
2023-08-09 23:21:03 +0800

    modified:   platformio.ini 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,38 +3,48 @@
 
 #include <Blinker.h>
 
-char auth[] = "Your MQTT Secret Key";
-char ssid[] = "Your WiFi network SSID or name";
-char pswd[] = "Your WiFi network WPA password or WEP key";
+char auth[] = "5c2248c553ad";
+char ssid[] = "LGY6";
+char pswd[] = "LGYy35286";
 
 #define BUTTON_1 "ButtonKey"
+// 新建组件对象
+BlinkerButton Button1("btn-abc");
+BlinkerNumber Number1("num-abc");
 
-void setup() {
-    Serial.begin(115200);
+int counter = 0;
 
-    // pinMode(12, OUTPUT);
-    // digitalWrite(12, LOW);
-
-    Blinker.begin(auth, ssid, pswd);
-    Blinker.wInit(BUTTON_1, W_BUTTON);
+// 按下按键即会执行该函数
+void button1_callback(const String & state) {
+    BLINKER_LOG("get button state: ", state);
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
 
-void loop()
+// 如果未绑定的组件被触发，则会执行其中内容
+void dataRead(const String & data)
 {
+    BLINKER_LOG("Blinker readString: ", data);
+    counter++;
+    Number1.print(counter);
+}
+
+void setup() {
+    // 初始化串口
+    Serial.begin(115200);
+
+    #if defined(BLINKER_PRINT)
+        BLINKER_DEBUG.stream(BLINKER_PRINT);
+    #endif
+    
+    // 初始化有LED的IO
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    // 初始化blinker
+    Blinker.begin(auth, ssid, pswd);
+    Blinker.attachData(dataRead);
+    Button1.attach(button1_callback);
+}
+
+void loop() {
     Blinker.run();
-
-    if (Blinker.available()) {
-        BLINKER_LOG2("Blinker.readString(): ", Blinker.readString());
-
-        uint32_t BlinkerTime = millis();
-
-        Blinker.beginFormat();
-        Blinker.vibrate();        
-        Blinker.print("millis", BlinkerTime);
-        Blinker.endFormat();
-    }
-
-    if (Blinker.button(BUTTON_1)) {
-        // digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    }
 }
